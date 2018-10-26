d6ef1c1a099b6c3f580ff121f77dfff5c2757293
sarpaul <43501744+sarpaul@users.noreply.github.com>
2018-10-26 22:27:39 +0300

    Add files via upload

---
+++ src/main/java/com/staff/api/entity/Interview.java
@@ -0,0 +1,52 @@
+package com.staff.api.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Interview implements IEntity<Interview> {
+
+    Integer id;
+    Integer idVacancy;
+    Integer idCandidate;
+    Date planDate; //Date
+    Date factDate; //Date
+
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
+    public Integer getId() {  return id; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getIdVacancy() { return idVacancy; }
+    public void setIdVacancy(Integer idVacancy)  { this.idVacancy = idVacancy; }
+    public Integer getIdCandidate() { return idCandidate; }
+    public void setIdCandidate(Integer idCandidate) { this.idCandidate = idCandidate; }
+    public Date getPlanDate() { return planDate; }
+    public void setPlanDate(Date planDate) { this.planDate = planDate; }
+    public Date getFactDate() { return factDate; }
+    public void setFactDate(Date factDate) { this.factDate = factDate; }
+
+
+    @Override
+    public String toString() {
+        return "Interview [id=" + id + ", idVacancy=" + idVacancy + ", planDate=" + planDate + ", idCandidate=" + idCandidate
+                + ", factDate=" + factDate + "]" + isNew();
+    }
+
+    @Override
+    public void setForeignKey(String foreignKey) {
+        this.id = Integer.parseInt(foreignKey);
+    }
+
+    @Override
+    public String getForeignKey() {
+        /*TODO: не думаю, что это правильно, но не придумал, как сделать лучше*/
+        return this.id != null ? this.id.toString() : "0000000000000000000000000000";
+    }
+
+}
