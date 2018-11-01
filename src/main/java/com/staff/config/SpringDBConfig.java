a9ab7e4cb917522d2cddfbe1f4de6003c0b81530
Roman <arrumanov@mail.ru>
2018-11-01 17:59:29 +0300

    commit empty

--- src/main/java/com/staff/config/SpringDBConfig.java
+++ src/main/java/com/staff/config/SpringDBConfig.java
@@ -30,7 +30,7 @@ public class SpringDBConfig {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUrl("jdbc:mysql://localhost:3306/team6?useSll=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setPassword("");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		return dataSource;
 	}
